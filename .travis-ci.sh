facb6d250302fd5d44f868232f4fa13897f5dc1e
Karl Palmskog <palmskog@gmail.com>
2017-05-25 22:20:20 -0500

    use local OPAM pin in Travis

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -7,12 +7,5 @@ opam repo add coq-released https://coq.inria.fr/opam/released
 opam repo add distributedcomponents-dev http://opam-dev.distributedcomponents.net
 
 opam pin add coq $COQ_VERSION --yes --verbose
-opam pin add coq-mathcomp-ssreflect $SSREFLECT_VERSION --yes --verbose
 
-opam install StructTact verdi cheerios --yes --verbose
-
-case $MODE in
-  *)
-    ./build.sh
-    ;;
-esac
+opam pin add verdi-cheerios . --yes --verbose
